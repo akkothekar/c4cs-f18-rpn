47a68229f982ae6927c604562b666b8e4ef5c762
akik <akik@umich.edu>
2018-10-26 15:09:08 -0400

    added subtraction test

--- rpn.py
+++ rpn.py
@@ -1,5 +1,22 @@
 def calculate(arg):
-    pass
+    stack = []
+    
+    tokens = arg.split()
+
+    for token in tokens:
+        try:
+            stack.append(int(token))
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            if token == '+':
+                result = val1 + val2
+            if token == '-':
+                result = val1 - val2
+
+            stack.append(result)
+
+    return stack[0]
 
 def main():
     while True:
